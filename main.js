1338e03bff399bc1fa4907c66ce7ac46fe451ad6
Sruthi Vinukonda <vinukonda.sruthi@gmail.com>
2018-05-13 19:24:41 -0500

    Add quotes

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-
+var quotes = ['Reality continues to ruin my life.',
+              'I\'m killing time while I wait for life to shower me with meaning and happiness.',
+              'When life gives you lemons, chunk it right back.',
+              'I\'m not dumb. I just have a command of thoroughly useless information.',
+              'Life\'s a lot more fun when you aren\'t responsible for your actions.',
+              'A day can really slip by when you\'re deliberately avoiding what you\'re supposed to do.',
+              'I have all these great genes, but they\'re recessive. That\'s the problem here.',
+              'Until you stalk and overrun, you cannot devour anyone.',
+              'I\'m leaking brain lubricant.'
+              'My book is called, Shut Up And Stop Whining: How To Do Something With Your Life Besides Think About Yourself.'
+            ]
