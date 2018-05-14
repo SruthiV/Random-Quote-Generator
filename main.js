bf4e0fabe6abc8f25d5e0e9b9bef0b4d527f2ab2
Sruthi Vinukonda <vinukonda.sruthi@gmail.com>
2018-05-13 19:44:49 -0500

    Add functionality

--- main.js
+++ main.js
@@ -6,6 +6,11 @@ var quotes = ['Reality continues to ruin my life.',
               'A day can really slip by when you\'re deliberately avoiding what you\'re supposed to do.',
               'I have all these great genes, but they\'re recessive. That\'s the problem here.',
               'Until you stalk and overrun, you cannot devour anyone.',
-              'I\'m leaking brain lubricant.'
+              'I\'m leaking brain lubricant.',
               'My book is called, Shut Up And Stop Whining: How To Do Something With Your Life Besides Think About Yourself.'
             ]
+
+function newQuote() {
+  var randomNumber = Math.floor(Math.random() * (quotes.length));
+  document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
+}
