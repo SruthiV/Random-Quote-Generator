309eea08f9be13ab21998ceec0b8bb0b0d003dd4
Sruthi Vinukonda <vinukonda.sruthi@gmail.com>
2018-08-19 19:26:24 -0500

    Add quotes

--- main.js
+++ main.js
@@ -7,7 +7,17 @@ var quotes = ['Reality continues to ruin my life.',
               'I have all these great genes, but they\'re recessive. That\'s the problem here.',
               'Until you stalk and overrun, you cannot devour anyone.',
               'I\'m leaking brain lubricant.',
-              'My book is called, Shut Up And Stop Whining: How To Do Something With Your Life Besides Think About Yourself.'
+              'My book is called, Shut Up And Stop Whining: How To Do Something With Your Life Besides Think About Yourself.',
+              'It\'s not denial. I\'m just selective about the reality I accept.',
+              'There\'s never enough time to do all the nothing you want.',
+              'In my opinion, we don\'t devote nearly enough scientific research to finding a cure for jerks.',
+              'Who was the guy who first looked at a cow and said \'I think I\’ll drink whatever comes out of these when I squeeze ’em?',
+              'I go to school, but I never learn what I want to know.',
+              'I\'m learning skills I will use for the rest of my life by doing homework...procrastinating and negotiation.',
+              'I find my life is a lot easier the lower I keep my expectations.',
+              'God put me on earth to accomplish certain things. Right now, I’m so far behind, I’ll never die.',
+              'You can drag my body to school but my spirit refuses to go.',
+              'I\'m not dumb. I just have a command of thoroughly useless information.'
             ]
 
 function newQuote() {
